5cd9cd37707fa1ca84d23a64c98bda6117917c2d
franciscorodriguezsv24 <franciscorodriguezsv24@gmail.com>
2025-08-13 16:59:55 -0600

    feat: create layout base for board

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,6 +1,23 @@
+import { ChartLine, PanelsTopLeft, UserRoundCheck, ClipboardList, Users, BookKey, SquareStack, Timer, PanelLeftOpen } from "lucide-react";
 import styles from './sidebar.module.scss'
 import { Link, useLocation } from 'react-router';
 import type { SidebarType } from '../../mock/sidebar';
+import profile from '../../assets/profile.jpg'
+
+const IconElement = (iconName: string, className: string) => {
+
+  const icons = {
+    timeline: <ChartLine className={className} size={20} />,
+    clients: <UserRoundCheck className={className} size={20}/>,
+    dashboard: <PanelsTopLeft className={className} size={20}/>,
+    groups: <SquareStack className={className} size={20}/>,
+    projects: <ClipboardList className={className} size={20}/>,
+    team: <BookKey className={className} size={20}/>,
+    teamMember: <Users className={className} size={20}/>
+  }
+
+  return icons[iconName as keyof typeof icons] || null;
+}
 
 export const Sidebar = ({ nav }: { nav: SidebarType[] }) => {
     const location = useLocation();
@@ -8,23 +25,35 @@ export const Sidebar = ({ nav }: { nav: SidebarType[] }) => {
 
   return (
     <div className={styles.sidebarContainer}>
+      <div>
         <div className={styles.sidebarTitleContainer}>
-            <img src='/icons/clock.svg' alt='clock'/>
-            <p>Resource Planning</p>
+          <div className={styles.sidebarTitle}>
+              <Timer size={20}/>
+              <p className={styles.sidebarTitleP}>Resource Planning</p>
+          </div>
+          <button className={styles.titleButton}>
+            <PanelLeftOpen size={18}/>
+          </button>
         </div>
         <div className={styles.navContainer}>
         {
-          nav.map((item, index) => {
+          nav.map((item) => {
             return (
-                <Link to={item.link ? item.link : "#"} className={` ${currentPath === item.link ? styles.selectedLinkTo : styles.linkTo } `} key={index}>
-                    <img src={item.icon} alt={item.label || "Navigation icon"} />
+                <Link to={item.link ? item.link : "#"} className={` ${currentPath === item.link ? styles.selectedLinkTo : styles.linkTo } `} key={item.label}>
+                    {IconElement(item.iconName, styles.iconClass)}
                     {item.label ? item.label: "hopping"}
                 </Link>
-            )
-          })
-        }
+              )
+            })
+          }
+        </div>
+      </div>
+      <div className={styles.sidebarFooter}>
+        <img src={profile} alt="user" className={styles.imgSidebar}/>
+        <p className={styles.sidebarP}>Alejandro Rodriguez</p>
       </div>
     </div>
+
   )
 }
 
--- src/page/home/index.tsx
+++ src/page/home/index.tsx
@@ -1,13 +1,63 @@
 import { Sidebar } from '../../components/sidebar'
+import { SearchIcon, PlusIcon, Settings2, ListFilter, Rows3, Grid2X2} from 'lucide-react'
 import styles from './home.module.scss'
 import { SidebarMock } from '../../mock/sidebar'
+import { useState } from 'react'
+import { Card } from '../../components/card'
 
 export const Home = () => {
+
+  const [value, setValue] = useState('')
+  const [viewType, setViewType] = useState('')
+  const [isFilter, setIsFilter] = useState(false)
+  const [view, setView] = useState(false)
+
   return (
     <div className={styles.homeContainer}>
         <Sidebar nav={SidebarMock}/>
-        <div>
+        <div className={styles.homeDashboardContainer}>
+          <div className={styles.homeDashboardHead}>
+            <h2>Clients</h2>
+            <div className={styles.homeDashboardActions}>
+              <div className={styles.homeDashboardActionsInputs}>
+                <input className={styles.homeDashboardInput}></input>
+                <SearchIcon className={styles.buttonSearchIcon} size={16}/>
+              </div>
+              <button className={styles.homeDashboardButtonAction}>
+                <PlusIcon/>
+              </button>
+            </div>
+          </div>
+          <div className={styles.filtersContainer}>
+            <div className={styles.filterSelectContainer}>
+              <button className={styles.filterCategory} onClick={() => setIsFilter(!isFilter)}>
+                <ListFilter/>
+                {
+                  value || 'filter'
+                }
+              </button>
+              <div className={ isFilter ? styles.valueFilterBlock : styles.valuesFilterhidden}>
+                <button onClick={() => {setValue('Active'); setIsFilter(!isFilter)}}>Active</button>
+                <button onClick={() => {setValue('Prospective'); setIsFilter(!isFilter)}}>Prospective</button>
+                <button onClick={() => {setValue('Archived'); setIsFilter(!isFilter)}}>Archived</button>
+              </div>
+            </div>
 
+            <div className={styles.valueDisplayCardContainer}>
+              <button className={styles.filterDisplayView} onClick={() => setView(!view)}>     <Settings2/>
+                  {
+                    viewType || 'Display'
+                  }
+              </button>
+              <div className={view ? styles.valueDisplayCardBlock : styles.valueDisplayCardHidden}>
+                <button onClick={() => {setViewType('Row'); setIsFilter(!isFilter)}}><Rows3/>Row</button>
+                <button onClick={() => {setViewType('Grid'); setIsFilter(!isFilter)}}><Grid2X2/>Grid</button>
+              </div>
+            </div>
+          </div>
+          <div className={styles.cardsContianer}>
+            <Card/>
+          </div>
         </div>
     </div>
   )
