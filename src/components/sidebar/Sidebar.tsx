2717f3d6cecd81bba02a7f96472df6f7f3319239
franciscorodriguezsv24 <franciscorodriguezsv24@gmail.com>
2025-08-15 14:51:32 -0600

    fix: fix document convention

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -3,6 +3,7 @@ import styles from './sidebar.module.scss'
 import { Link, useLocation } from 'react-router';
 import type { SidebarType } from '../../mock/sidebar';
 import profile from '../../assets/profile.jpg'
+import { useState } from "react";
 
 const IconElement = (iconName: string, className: string) => {
 
@@ -22,16 +23,17 @@ const IconElement = (iconName: string, className: string) => {
 export const Sidebar = ({ nav }: { nav: SidebarType[] }) => {
     const location = useLocation();
     const currentPath = location.pathname
+    const [changePosition, setChangePosition] = useState<boolean>(false)
 
   return (
-    <div className={styles.sidebarContainer}>
+    <div className={changePosition ? styles.sidebarContainerDynamic : styles.sidebarContainer}>
       <div>
         <div className={styles.sidebarTitleContainer}>
           <div className={styles.sidebarTitle}>
               <Timer size={20}/>
               <p className={styles.sidebarTitleP}>Resource Planning</p>
           </div>
-          <button className={styles.titleButton}>
+          <button className={styles.titleButton} onClick={() => setChangePosition(!changePosition)}>
             <PanelLeftOpen size={18}/>
           </button>
         </div>
@@ -39,7 +41,7 @@ export const Sidebar = ({ nav }: { nav: SidebarType[] }) => {
         {
           nav.map((item) => {
             return (
-                <Link to={item.link ? item.link : "#"} className={` ${currentPath === item.link ? styles.selectedLinkTo : styles.linkTo } `} key={item.label}>
+                <Link to={item.link} className={` ${currentPath === item.link ? styles.linkTo: styles.selectedLinkTo } `} key={item.label}>
                     {IconElement(item.iconName, styles.iconClass)}
                     {item.label ? item.label: "hopping"}
                 </Link>
--- src/page/home/Home.tsx
+++ src/page/home/Home.tsx
@@ -1,14 +1,21 @@
-import { Sidebar } from '../../components/sidebar'
+import { Sidebar } from '../../components/sidebar/Sidebar'
 import { SearchIcon, PlusIcon} from 'lucide-react'
 import styles from './home.module.scss'
 import { SidebarMock } from '../../mock/sidebar'
-import { Suspense,  } from 'react'
-import { Card } from '../../components/card'
+import { Suspense, useState,  } from 'react'
+import { Card } from '../../components/card/card' 
 import { FilterStatus } from '../../components/filter/FilterStatus'
 import { LayoutFilter } from '../../components/displayLayout/LayoutFilter'
 
 export function Home() {
 
+    const [value, setValue] = useState<string>('')
+    const [search, setSearch] = useState<string>('')
+    const[layout, setLayout] = useState<string>('')
+
+    function searchFilter(event: React.ChangeEvent<HTMLInputElement>) {
+      setSearch(event.target.value)
+    }
   return (
     <div className={styles.homeContainer}>
         <Sidebar nav={SidebarMock}/>
@@ -17,7 +24,7 @@ export function Home() {
             <h2>Clients</h2>
             <div className={styles.homeDashboardActions}>
               <div className={styles.homeDashboardActionsInputs}>
-                <input className={styles.homeDashboardInput}></input>
+                <input className={styles.homeDashboardInput} onChange={searchFilter} value={search}></input>
                 <SearchIcon className={styles.buttonSearchIcon} size={16}/>
               </div>
               <button className={styles.homeDashboardButtonAction}>
@@ -26,13 +33,16 @@ export function Home() {
             </div>
           </div>
           <div className={styles.filtersContainer}>
-            <FilterStatus/>
-            <LayoutFilter/>
+            <FilterStatus setValue={setValue} value={value}/>
+            <LayoutFilter layout={layout} setLayout={setLayout}/>
           </div>
-          <div className={styles.test}>
+          <div className={layout == "Grid" ? styles.CardsLayout : styles.CardsContainer}>
             <Suspense fallback={<FallBack/>}>
-              <Card/>
+              <Card value={value} search={search} layout={layout}/>
             </Suspense>
+          </div>
+          <div className={styles.footerContainer}>
+            Footer
           </div>
         </div>
     </div>
